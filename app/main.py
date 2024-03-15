fc9888ce61bf107704facaf110a1d3a423f5cfc4
Gomdue <hdh026163@gmail.com>
2024-03-16 03:52:15 +0900

    add Swagger details

--- app/main.py
+++ app/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 from model import model_router
 
-app = FastAPI()
+app = FastAPI(
+    title="MIDAS",
+    description="Object Detection and Object Classification API description for MIDAS",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
 
 app.include_router(model_router.app, tags=["model"])
 

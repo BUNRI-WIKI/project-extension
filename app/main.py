73a5fcf4d73c6d865815370a3ba922bc8f606724
GomDue <hdh026163@gmail.com>
2024-06-25 22:45:40 +0900

    chore: add api docs info & change logits to softmax

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.model import router
 app = FastAPI(
     title="MIDAS",
     description="Object Detection and Object Classification API description for MIDAS",
-    version="0.1.0",
+    version="0.2.0",
     docs_url="/docs",
     redoc_url="/redoc"
 )
